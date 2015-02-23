dcd2b426407c3bcaa599bc7b9850c6188dab4255
Soma Holiday <soma@somaholiday.com>
2015-02-23 20:43:12 +0100

    add Alt+Enter support to publish current editor
    The tracking of the editors is a little shaky right now...  based on the id passed in to addNewEditor.  We add a magic field 'joukkue_index' to the CodeMirror object so it's accessible from the extraKeys event handler.

---
+++ index.js
@@ -0,0 +1,86 @@
+// ShareJS connection
+var s = new BCSocket(null, {
+	reconnect: true
+});
+var sjs = new window.sharejs.Connection(s);
+
+function addNewEditor(room, layer, id) {
+	var ed = {};
+
+	// add new textarea to the editor
+	var column = document.getElementById("right_column");
+	var textarea = document.createElement("textarea");
+	textarea.id = id;
+	column.appendChild(textarea);
+
+	// attach CodeMirror to the new textarea
+	ed.elem = document.getElementById(id);
+	ed.cm = CodeMirror.fromTextArea(ed.elem, {
+	  //lineNumbers: true,
+	  matchBrackets: true,
+	  autoCloseBrackets: true,
+	  styleActiveLine: true,
+	  extraKeys: { 
+	    'Ctrl-Q': 'toggleComment',
+	    'Ctrl-Space': 'autocomplete',
+	    'Alt-Enter': 'joukkueEval'
+	  },
+	  tabSize: 2,
+	  theme: 'zenburn',
+	  mode: { name: 'javascript', json: false, globalVars: true }
+	});
+
+	// augment cm object with our index to find it later
+	ed.cm.joukkue_index = id;
+
+	// attach ShareJS to CodeMirror
+	ed.doc = sjs.get(room, layer);
+	ed.doc.subscribe();
+	ed.doc.whenReady(function () {
+	  if (!ed.doc.type) ed.doc.create('text');
+	  if (ed.doc.type && ed.doc.type.name === 'text') {
+	    ed.doc.attachCodeMirror(ed.cm);
+	  }
+	});
+
+	return ed;
+}
+
+//  ╔═╗╔═╕  ┬┌─┐
+//  ╠═╝╚═╗  │└─┐
+//  ╩  ╘═╝o└┘└─┘
+
+function setup() {
+  createCanvas(540, 540);
+  background(0);
+  loadImage("/public/joukkue.png", function(i) {
+    image(i, width/2-i.width/2, height/2-i.height/2)
+  });
+}
+
+function draw() {
+	for (var i=0; i < editors.length; i++) {
+		if (editors[i].code) {
+			editors[i].code();
+		}
+	}
+}
+
+//   ╦┌─┐┬ ┬┬┌─┬┌─┬ ┬┌─┐
+//   ║│ ││ │├┴┐├┴┐│ │├┤
+//  ╚╝└─┘└─┘┴ ┴┴ ┴└─┘└─┘
+
+var editors = [];
+for(var i=0; i<20; i++) {
+	editors.push(addNewEditor('room1', 'code'+i, i));
+}
+
+CodeMirror.commands.joukkueEval = function(cm) {
+	var code = cm.doc.getValue();
+	var which = int(cm.joukkue_index);
+	
+	var editor = editors[which];
+	eval('var f = function(d) { ' + code + ' }');
+  	editor.code = f;
+}
+
